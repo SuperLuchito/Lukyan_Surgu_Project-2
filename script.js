656ec309d845a4dac4ae05074b0a15d1d7296cdf
luchok503 <lukyannib@mail.ru>
2024-09-25 23:16:52 +0500

    Release #1.2: "Updating the JS code"

--- script.js
+++ script.js
@@ -1,77 +1,106 @@
-const inputEl = (document.getElementsByClassName('app__controls-input'))[0]
-const btnEl = (document.getElementsByClassName('app__controls-button'))[0]
-const listEl = (document.getElementsByClassName('app__list'))[0]
+const inputEl = (document.getElementsByClassName('app__controls-input'))[0];
+const btnEl = (document.getElementsByClassName('app__controls-button'))[0];
+const listEl = (document.getElementsByClassName('app__list'))[0];
 
-let counter = 1
+let counter = 1;
 
-const data = []
+function loadData() {
+    const savedData = localStorage.getItem('tasks');
+    return savedData ? JSON.parse(savedData) : [];
+}
+
+const data = loadData();
 
 data.forEach((item) => {
-    if (item.id > counter) {
-        counter = item.id + 1
+    if (item.id >= counter) {
+        counter = item.id + 1;
     }
-})
+});
 
-if (counter > 1) {
-    counter++
+function saveData() {
+    localStorage.setItem('tasks', JSON.stringify(data));
 }
 
 function createTask(objectData) {
-    const root = document.createElement('div')
-    root.classList.add('app__list-item')
+    const root = document.createElement('div');
+    root.classList.add('app__list-item');
 
     if (objectData.isDone) {
-        root.classList.add('app__list-item_done')
+        root.classList.add('app__list-item_done');
     }
 
-    const input = document.createElement('input')
-    input.classList.add('app__list-checkbox')
-    input.type = 'checkbox'
+    const input = document.createElement('input');
+    input.classList.add('app__list-checkbox');
+    input.type = 'checkbox';
 
     if (objectData.isDone) {
-        input.checked = true
+        input.checked = true;
     }
 
-    const txt = document.createElement('p')
-    txt.classList.add('app__list-item-txt')
-    txt.innerText = objectData.text
+    const txt = document.createElement('p');
+    txt.classList.add('app__list-item-txt');
+    txt.innerText = objectData.text;
 
-    const btn = document.createElement('button')
-    btn.classList.add('app__list-btn')
+    const btn = document.createElement('button');
+    btn.classList.add('app__list-btn');
 
-    const img = document.createElement('img')
-    img.src = '/images/image_trash-bin.svg'
-    img.alt = ''
+    const img = document.createElement('img');
+    img.src = '/images/image_trash-bin.svg';
+    img.alt = '';
 
-    btn.appendChild(img)
+    btn.appendChild(img);
 
-    root.appendChild(input)
-    root.appendChild(txt)
-    root.appendChild(btn)
+    btn.addEventListener('click', (event) => {
+        event.stopPropagation();
+        deleteTask(objectData.id);
+    });
 
-    return root
+    root.addEventListener('click', () => toggleTaskState(objectData.id));
+
+    root.appendChild(input);
+    root.appendChild(txt);
+    root.appendChild(btn);
+
+    return root;
+}
+
+function deleteTask(id) {
+    const index = data.findIndex(item => item.id === id);
+    if (index !== -1) {
+        data.splice(index, 1);
+        saveData();
+        render();
+    }
+}
+
+function toggleTaskState(id) {
+    const task = data.find(item => item.id === id);
+    if (task) {
+        task.isDone = !task.isDone;
+        saveData();
+        render();
+    }
 }
 
 btnEl.addEventListener('click', () => {
-    const textValue = inputEl.value
+    const textValue = inputEl.value;
     data.push({
         id: counter++,
         text: textValue,
         isDone: false
-    })
-    render()
-    console.log(data);
+    });
+    saveData();
+    render();
     
-    inputEl.value = ''
-})
+    inputEl.value = '';
+});
 
 function render() {
-    listEl.innerHTML = ''
+    listEl.innerHTML = '';
     for (let item of data) {
-        const tmpEl = createTask(item)
-        listEl.appendChild(tmpEl)
+        const tmpEl = createTask(item);
+        listEl.appendChild(tmpEl);
     } 
 }
 
-render()
-
+render();
